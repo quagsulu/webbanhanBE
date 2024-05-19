0c2d26d4ba5986c0904b878517190b2bfd73fe4f
quagsulu <quangnvph24106@fpt.edu.vn>
2024-05-19 15:25:58 +0700

    sửa lại logic

--- src/models/Category.js
+++ src/models/Category.js
@@ -5,40 +5,41 @@ const categorySchema = mongoose.Schema(
     name: {
       type: String,
       required: true
-    },
-    isDeleteable: {
-      type: Boolean,
-      required: true,
-      default : true
-    },
-    products: [{
-      type: mongoose.Types.ObjectId,
-      ref: 'Movie'
-    }],
-    slug : {
-      type : String
     }
+    // ,
+    // isDeleteable: {
+    //   type: Boolean,
+    //   required: true,
+    //   default : true
+    // },
+    // products: [{
+    //   type: mongoose.Types.ObjectId,
+    //   ref: 'Movie'
+    // }],
+    // slug : {
+    //   type : String
+    // }
   },
   { versionKey: false, timestamps: true }
 );
 
-categorySchema.pre('findOneAndDelete', async function (next) {
-  try {
-    // Lấy model Product từ biến đã importc
-    const Product = mongoose.model('Movie')
-    // Lấy điều kiện tìm kiếm hiện tại của câu lệnh , xác định category
-    const filter = this.getFilter()
+// categorySchema.pre('findOneAndDelete', async function (next) {
+//   try {
+//     // Lấy model Product từ biến đã importc
+//     const Product = mongoose.model('Movie')
+//     // Lấy điều kiện tìm kiếm hiện tại của câu lệnh , xác định category
+//     const filter = this.getFilter()
 
-    await Product.updateMany(
-      { categoryId : filter._id },
-      { $pull : { categoryId : filter._id } }
-    )
-    next()
-  } catch (error) {
-    next(error)
-  }
+//     await Product.updateMany(
+//       { categoryId : filter._id },
+//       { $pull : { categoryId : filter._id } }
+//     )
+//     next()
+//   } catch (error) {
+//     next(error)
+//   }
 
-})
+// })
 categorySchema.plugin(mongoosePaginate);
 
 export default mongoose.model('Category', categorySchema);
--- src/validate/category.js
+++ src/validate/category.js
@@ -2,8 +2,8 @@ import Joi from 'joi';
 
 const categorySchema = Joi.object({
   name: Joi.string().required().min(1).trim().strict(),
-  isDeleteable: Joi.boolean(),
-  products: Joi.array().items(Joi.string())
+  // isDeleteable: Joi.boolean(),
+  // products: Joi.array().items(Joi.string())
 }).options({
   abortEarly: false
 });
